2532f4dc3017edfe5dd90325ccb6e771243e2d19
Kadiri Naveen Reddy <naveenbgp2000@gmail.com>
2026-05-02 14:05:51 +0530

    updated test

--- tests/UI/login.spec.js
+++ tests/UI/login.spec.js
@@ -2,22 +2,15 @@ import { test, expect } from '@playwright/test';
 import {ENV} from '../../config/envConfig';
 import { LoginPage } from '../../pages/login';
 
-let loginpage,loginpage1;
+let loginpage;
 test('login with valid credentials', async ({ browser }) => {
     // const browser=new chromium.lunch();
      const context=await browser.newContext();
-    const context2=await browser.newContext();
     const page=await context.newPage();
-    const page1=await context2.newPage()
     loginpage =new LoginPage(page);
-    loginpage1 =new LoginPage(page1);
     await loginpage.navigateToLoginPage();
-     await loginpage1.navigateToLoginPage();
-    console.log(ENV.USERNAME)
     await loginpage.enterUserNameAndPassword(ENV.USERNAME,ENV.PASSWORD);
-    await loginpage1.enterUserNameAndPassword(ENV.USERNAME,ENV.PASSWORD);
     await loginpage.clickOnLoginButton()
-     await loginpage1.clickOnLoginButton()
    
 
 })
